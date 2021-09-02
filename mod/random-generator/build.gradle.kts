51e542618374faefc955df764bb0a7728cad8f67
Ilya Buhlakou <neonlightnight@gmail.com>
2021-09-02 22:35:18 +0300

    initial commit

---
+++ mod/random-generator/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "com.ilyabuhlakou.mod"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
